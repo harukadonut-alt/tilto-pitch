628df90d44229d9a914a7fbb4a744226b2e5e879
松田はるか <matsudaharuka@matsudaharukanoMacBook-Air.local>
2026-09-08 17:50:16 +0900

    v4: 05図面の回す軸をやり直し。縦軸の回しを0に戻し、横軸まわりを27度に

---
+++ v4/assets/layout-segment-context-D8kpiUqc.1c1e2160.js
@@ -0,0 +1,1 @@
+import{r as e}from"./rolldown-runtime-S-ySWqyJ.1c1e2160.js";import{i as t}from"./framework-DjPHiq1u.1c1e2160.js";import{t as n}from"./index-DteDOMqj.1c1e2160.js";var r=e(t(),1);function i({segmentMap:e,children:t}){let i=n();return i?(0,r.createElement)(i.Provider,{value:e},t):t}export{i as LayoutSegmentProvider};
